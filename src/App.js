a1c4b24bfbdcc20dea4f6120afcf06a64da4225a
Digambaran <arjunsreekumar.1@gmail.com>
2022-08-19 19:30:27 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Erwer from './erwer'
+export default function App() {
+  return (
+    <div>
+      <Erwer/>
+    </div>
+  )
+}
---
+++ src/erwer.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const Erwer = () => {
+  return (
+   <p>Hello from erwer </p>
+  )
+}
+export default Erwer
